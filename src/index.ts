f6de1d6d89577f7cdb8f4dc147ad3d4e68e48b14
kde1989 <k.d.e.1989@gmail.com>
2023-05-02 17:13:29 +0300

    add videos

--- src/index.ts
+++ src/index.ts
@@ -2,9 +2,26 @@ import express, {Request, Response} from 'express'
 
 const app = express()
 const port = process.env.PORT || 3000
+const videos = [
+    {
+        "id": 0,
+        "title": "string",
+        "author": "string",
+        "canBeDownloaded": true,
+        "minAgeRestriction": null,
+        "createdAt": "2023-05-02T14:09:09.441Z",
+        "publicationDate": "2023-05-02T14:09:09.441Z",
+        "availableResolutions": [
+            "P144"
+        ]
+    }
+]
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Online cinema "Platinum"')
+})
+app.get('/videos', (req: Request, res: Response) => {
+    res.send(videos)
 })
 
 app.listen(port, () => {
