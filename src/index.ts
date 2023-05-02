1eab8ba899789d0dbb138d62f44396b05b4c3d39
kde1989 <k.d.e.1989@gmail.com>
2023-05-02 14:53:56 +0300

    Fix port

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
 import express, {Request, Response} from 'express'
 
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Online cinema "Platinum"')
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
+    console.log(`Example app listening on port: ${port}`)
 })
