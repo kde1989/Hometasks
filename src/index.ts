c6c9d2e4386d72b2842f667b83ccad8fcd0b0535
kde1989 <k.d.e.1989@gmail.com>
2023-05-02 14:14:33 +0300

    New project

--- src/index.ts
+++ src/index.ts
@@ -1,24 +1,12 @@
-import express, {Request, Response} from 'express';
-import bodyParser from 'body-parser'
+import express, {Request, Response} from 'express'
 
-const app = express();
-const port = 3000;
-const videos = {
-        "id": 0,
-        "title": "string",
-        "author": "string",
-        "canBeDownloaded": true,
-        "minAgeRestriction": null,
-        "createdAt": "string",
-        "publicationDate": "string",
-        "availableResolutions": [
-            "P144"
-        ]
-    }
+const app = express()
+const port = 3000
 
-app.get('/videos', (req: Request, res: Response) => {
-    res.send(videos)
-});
+app.get('/', (req: Request, res: Response) => {
+    res.send('Online cinema "Platinum"')
+})
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
-});
+})
