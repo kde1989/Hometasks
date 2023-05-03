e3af057a89e0c33cc1ad35f4b1fcea484b61aff8
kde1989 <k.d.e.1989@gmail.com>
2023-05-03 12:57:57 +0300

    Add CRUD

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,74 @@
 import express, {Request, Response} from 'express'
+import bodyParser from 'body-parser';
 
 const app = express()
 const port = process.env.PORT || 3000
 const videos = [
     {
-        "id": 0,
-        "title": "string",
-        "author": "string",
-        "canBeDownloaded": true,
-        "minAgeRestriction": null,
-        "createdAt": "2023-05-02T14:09:09.441Z",
-        "publicationDate": "2023-05-02T14:09:09.441Z",
-        "availableResolutions": [
+        id: 1,
+        title: 'matrix',
+        author: "string",
+        canBeDownloaded: true,
+        minAgeRestriction: null,
+        createdAt: "2023-05-02T14:09:09.441Z",
+        publicationDate: "2023-05-02T14:09:09.441Z",
+        availableResolutions: [
             "P144"
         ]
-    }
+    },
 ]
+const parserMiddleware = bodyParser({})
+
+app.use(parserMiddleware)
 
 app.get('/', (req: Request, res: Response) => {
     res.send('Online cinema "Platinum"')
 })
 app.get('/videos', (req: Request, res: Response) => {
     res.send(videos)
+})
+app.post('/videos', (req: Request, res: Response) => {
+    const newVideo = {
+            id: 1,
+            title: 'matrix',
+            author: "string",
+            canBeDownloaded: true,
+            minAgeRestriction: null,
+            createdAt: "2023-05-02T14:09:09.441Z",
+            publicationDate: "2023-05-02T14:09:09.441Z",
+            availableResolutions: [
+                "P144"
+            ]
+        }
+    videos.push(newVideo)
+    res.send(201).send(newVideo)
+})
+app.get('/videos/:id', (req: Request, res: Response) => {
+    let video = videos.find(v => v.id === +req.params.id)
+    if(video) {
+        res.send(video)
+    } else {
+        res.send(404)
+    }
+})
+app.put('/videos/:id', (req: Request, res: Response) => {
+    let video = videos.find(v => v.id === +req.params.id)
+    if(video) {
+        video.title = req.body.title
+        res.send(video)
+    } else {
+        res.send(404)
+    }
+})
+app.delete('/videos/:id', (req: Request, res: Response) => {
+    for (let i = 0; i < videos.length; i++) {
+        if(videos[i].id === +req.params.id) {
+            videos.splice(i, 1);
+            res.send(204)
+            return;
+        }
+    }
+    res.send(404)
 })
 
 app.listen(port, () => {
