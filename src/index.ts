9a981311ec73ca5dd9dcda9dbd21f61b89b501a3
kde1989 <k.d.e.1989@gmail.com>
2023-05-09 18:31:33 +0300

    Fix errors

--- src/index.ts
+++ src/index.ts
@@ -1,19 +1,22 @@
-import express, {Request, Response} from 'express'
+import express, {Request, Response} from 'express';
+import bodyParser from "body-parser";
+import cors from 'cors';
 
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 
-type videoType = {
-    id: number,
-    title: string,
-    author: string,
-    canBeDownloaded: boolean,
-    minAgeRestriction: number | null
-    createdAt: string,
-    publicationDate: string,
-    availableResolutions: string[]
-}
-let videos: videoType[] = [
+const corsMiddleware = cors();
+app.use(corsMiddleware);
+const jsonBodyMiddleware = bodyParser.json();
+app.use(jsonBodyMiddleware);
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('<h1>Online cinema "Platinum"</h1>')
+});
+
+const availableResolutions = ["P144", "P240", "P360", "P480", "P720", "P1080", "P1440", "P2160"];
+
+let videos = [
     {
         id: 1,
         title: "The Matrix",
@@ -38,87 +41,138 @@ let videos: videoType[] = [
             "1080"
         ]
     }
-]
+];
 
-enum availableResolutions {
-    P144 = "P144",
-    P240 = "P240",
-    P360 = "P360",
-    P480 = "P480",
-    P720 = "P720",
-    P1080 = "P1080",
-    P1440 = "P1440",
-    P2160 = "P2160"
-}
+app.delete('/testing/all-data', (req: Request, res: Response) => {
+    videos.splice(0, videos.length);
+    res.sendStatus(204)
+});
 
-const jsonBodyMiddleware = express.json()
-app.use(jsonBodyMiddleware)
-
-app.get('/', (req: Request, res: Response) => {
-    res.send('<h1>Online cinema "Platinum"</h1>')
-})
 app.get('/videos', (req: Request, res: Response) => {
-    res.json(videos)
-})
+    res.send(videos)
+});
 app.get('/videos/:id', (req: Request, res: Response) => {
-    const foundVideo = videos.find(videos => videos.id === +req.params.id);
-
-    if (!foundVideo) {
-        res.sendStatus(404);
-        return;
+    let foundVideo = videos.find(videos => videos.id === +req.params.id);
+    if (foundVideo) {
+        res.send(foundVideo)
+    } else {
+        res.sendStatus(404)
     }
-
-    res.json(foundVideo)
-})
+});
 app.post('/videos', (req: Request, res: Response) => {
-    const createdAt = new Date()
-    const publicationDate = new Date(createdAt.getTime() + 86400000)
-    let createdVideo:videoType = {
+    let createdVideo = {
         id: +(new Date()),
         title: req.body.title,
         author: req.body.author,
-        canBeDownloaded: true,
-        minAgeRestriction: null,
-        createdAt: createdAt.toISOString(),
-        publicationDate: publicationDate.toISOString(),
-        availableResolutions: ['availableResolutions']
-    }
-    videos.push(createdVideo)
-    res.status(201).json(createdVideo);
+        canBeDownloaded: req.body.canBeDownloaded,
+        minAgeRestriction: req.body.minAgeRestriction,
+        createdAt: (new Date().toISOString()),
+        publicationDate: (new Date(new Date().setDate(new Date().getDate() + 1)).toISOString()),
+        availableResolutions: req.body.availableResolutions
+    };
+    let errors = [];
 
-    if (!req.body.title) {
-        res.sendStatus(400)
-        return;
+    if (typeof createdVideo.title !== 'string' || createdVideo.title.length > 40) {
+        errors.push({message: 'error', field: 'title'})
     }
-})
+    if (typeof createdVideo.author !== 'string' || createdVideo.author.length > 20) {
+        errors.push({message: 'error', field: 'author'})
+    }
+    if (Array.isArray(createdVideo.availableResolutions)) {
+        const length = createdVideo.availableResolutions.length;
+        let checking = createdVideo.availableResolutions.filter(value => {
+            return availableResolutions.includes(value)
+        })
+        if (checking.length < length) {
+            errors.push({message: 'error', field: 'availableResolutions'})
+        }
+    } else {
+        errors.push({message: 'error', field: 'availableResolutions'})
+    }
+    if (typeof createdVideo.canBeDownloaded !== "boolean") {
+        if (createdVideo.canBeDownloaded === undefined) {
+            createdVideo.canBeDownloaded = false
+        } else {
+            errors.push({message: 'error', field: 'canBeDownloaded'})
+        }
+    }
+    if (createdVideo.minAgeRestriction !== null && typeof createdVideo.minAgeRestriction !== "number") {
+        if (createdVideo.minAgeRestriction === undefined) {
+            createdVideo.minAgeRestriction = null
+        } else {
+            errors.push({message: 'error', field: 'canBeDownloaded'})
+        }
+    } else if (typeof createdVideo.minAgeRestriction === "number") {
+        if (createdVideo.minAgeRestriction < 1 || +createdVideo.minAgeRestriction > 18) {
+            errors.push({message: 'error', field: 'canBeDownloaded'})
+        }
+    }
+    if (errors.length > 0) {
+        let errorsList = {errorsMessages: errors};
+        res.status(400).send(errorsList)
+    } else {
+        videos.push(createdVideo);
+        res.status(201).json(createdVideo)
+    }
+});
 app.put('/videos/:id', (req: Request, res: Response) => {
-    const foundVideo = videos.find(videos => videos.id === +req.params.id);
-    if (!foundVideo) {
-        res.sendStatus(404)
-        return;
+    let foundVideo = videos.find(videos => videos.id === +req.params.id);
+    let index = videos.findIndex(videos => videos.id === +req.params.id);
+    let errors = [];
+    if (foundVideo) {
+        const newVideo = {...foundVideo, ...req.body};
+        if (typeof newVideo.title !== 'string' || newVideo.title.length > 40) {
+            errors.push({message: 'error', field: 'title'})
+        }
+        if (typeof newVideo.author !== 'string' || newVideo.author.length > 20) {
+            errors.push({message: 'error', field: 'author'})
+        }
+        if (Array.isArray(newVideo.availableResolutions)) {
+            const length = newVideo.availableResolutions.length;
+            let checking = newVideo.availableResolutions.filter((value: string) => {
+                return availableResolutions.includes(value)
+            })
+            if (checking.length < length) {
+                errors.push({message: 'error', field: 'availableResolutions'})
+            }
+        } else {
+            errors.push({message: 'error', field: 'availableResolutions'})
+        }
+        if (typeof newVideo.canBeDownloaded !== "boolean") {
+            errors.push({message: 'error', field: 'canBeDownloaded'})
+        }
+        if (newVideo.minAgeRestriction !== null && typeof newVideo.minAgeRestriction !== "number") {
+            errors.push({message: 'error', field: 'canBeDownloaded'})
+        } else if (typeof newVideo.minAgeRestriction === "number") {
+            if (+newVideo.minAgeRestriction < 1 || +newVideo.minAgeRestriction > 18) {
+                errors.push({message: 'error', field: 'canBeDownloaded'})
+            }
+        }
+        if (typeof newVideo.publicationDate !== "string") {
+            errors.push({message: 'error', field: 'publicationDate'})
+        }
+        if (errors.length > 0) {
+            let errorsList = {errorsMessages: errors};
+            res.status(400).send(errorsList)
+        } else {
+            videos[index] = newVideo;
+            res.status(204)
+        }
+    } else {
+        res.status(404)
     }
-    foundVideo.title = req.body.title;
-    res.sendStatus(204)
-
-    if (!req.body.title) {
-        res.sendStatus(400)
-        return;
-    }
-})
+});
 app.delete('/videos/:id', (req: Request, res: Response) => {
-    const id = +req.params.id;
-    const newVideos = videos.filter(videos => videos.id !== id)
+    let id = +req.params.id;
+    let newVideos = videos.filter(videos => videos.id !== id);
     if (newVideos.length < videos.length) {
         videos = newVideos
         res.sendStatus(204)
     } else {
         res.sendStatus(404)
     }
-})
-app.delete('/testing/all-data', (req: Request, res: Response) => {
-    const noVideos = []
-    res.sendStatus(204)
-})
+});
+
 app.listen(port, () => {
     console.log(`Example app listening on port ${port}`)
 })
